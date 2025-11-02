860d2311066da404bd0a754be71bd31ce856d6a5
Natalie K <53866967+nbkim481@users.noreply.github.com>
2025-11-02 19:57:46 +0000

    import cookie clicker site from codepen

---
+++ script.js
@@ -0,0 +1,11 @@
+var num = 0;
+
+var cookie = document.getElementById("cookie");
+
+function cookieClick() {
+  num += 1;
+  var number = document.getElementById("number");
+  number.innerHTML = num;
+  
+}
+
